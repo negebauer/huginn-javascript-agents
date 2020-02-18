ea4aadecf1949d25af6265ca77c9f3fa3cf9eb1f
Nicolas Gebauer <negebauerm@gmail.com>
2020-02-18 00:47:57 -0300

    chore: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const glob = require('glob')
+
+const agentsFolder = path.resolve(__dirname, 'src', 'agents')
+
+module.exports = {
+  entry: glob
+    .sync(`${agentsFolder}/*.js`)
+    .filter(name => !name.includes('.test.'))
+    .reduce(
+      (acc, filePath) => ({
+        ...acc,
+        [filePath.replace(`${agentsFolder}/`, '')]: filePath,
+      }),
+      {},
+    ),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+}
