3b8cfb56b2f37140527b0da7ba0ea52227cf863b
Nicolas Gebauer <negebauerm@gmail.com>
2020-02-19 19:19:58 -0300

    move agent test helpers

---
+++ src/agents/tests/helpers.js
@@ -0,0 +1,18 @@
+function defaultAgentMocks() {
+  return {
+    log: jest.fn(),
+    createEvent: jest.fn(),
+    memory: jest.fn(),
+  }
+}
+
+function mockAgentCall(fn, event, extraMocks = {}) {
+  const finalMocks = {
+    ...defaultAgentMocks(),
+    ...extraMocks,
+  }
+  fn({ Agent: finalMocks, event })
+  return finalMocks
+}
+
+module.exports = { mockAgentCall }
